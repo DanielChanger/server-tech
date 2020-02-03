a2fe86c96ee6e91489796dc03143771c2fd991cf
dminiail <dminiail@cisco.com>
2020-02-03 16:39:04 +0200

    Redone for php 7+

--- lab2.php
+++ lab2.php
@@ -1,49 +1,38 @@
 <?php
-$connection = new MongoClient();
-$connection->selectDB("profile");
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$bulk = new MongoDB\Driver\BulkWrite;
 
-try {
-    $applications = $connection->selectDB("profile")->selectCollection("applications");
-    $programmers = $connection->selectDB("profile")->selectCollection("programmers");
+$applicationsList = array(
+    array(
+        "name" => "flOw",
+        "createdAt" => new DateTime(),
+    ),
+    array(
+        "name" => "Flower",
+        "createdAt" => new DateTime()
+    ),
+    array(
+        "name" => "The Journey",
+        "createdAt" => new DateTime()
+    )
+);
 
-    $applicationsList = array(
-        array(
-            "name" => "flOw",
-            "createdAt" => new DateTime(),
-        ),
-        array(
-            "name" => "Flower",
-            "createdAt" => new DateTime()
-        ),
-        array(
-            "name" => "The Journey",
-            "createdAt" => new DateTime()
-        )
-    );
-
-    foreach ($applicationsList as $app) {
-        $applications->insert($app);
-    }
-
-    $programmer = array(
-        "first_name" => "Daniel",
-        "last_name" => "Miniailo",
-        "group" => "121-16-1",
-        "profile" => "Java Developer",
-        "Applications" => array(
-            MongoDBRef::create("applications", $applicationsList[0]['_id']),
-            MongoDBRef::create("applications", $applicationsList[1]['_id']),
-            MongoDBRef::create("applications", $applicationsList[2]['_id']),
-        )
-    );
-
-    $programmers->insert($programmer);
-
-
-} catch (Exception $e) {
-    echo "Something went wrong";
+foreach ($applicationsList as $app) {
+    $manager->executeBulkWrite('applications', $app);
 }
 
-$connection->close();
+$programmer = array(
+    "first_name" => "Daniel",
+    "last_name" => "Miniailo",
+    "group" => "121-16-1",
+    "profile" => "Java Developer",
+    "Applications" => array(
+        MongoDBRef::create('applications', $applicationsList[0]['_id']),
+        MongoDBRef::create('applications', $applicationsList[1]['_id']),
+        MongoDBRef::create('applications', $applicationsList[2]['_id']),
+    )
+);
+
+$manager->executeBulkWrite('programmers', $app);
 
 ?>
