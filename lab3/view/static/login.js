50688cf3b17d24c0b28fc51bd578b4ed875393d5
dminiail <dminiail@cisco.com>
2020-02-09 00:38:52 +0200

    prevent default

--- lab3/view/static/login.js
+++ lab3/view/static/login.js
@@ -1,13 +1,13 @@
-let loginForm;
-function addListeners() {
-    loginForm = document.getElementById("login-form");
-    loginForm.addEventListener('submit', (ev) => {
-        ev.preventDefault();
-        handleRequest();
-    });
-}
+// let loginForm;
+// function addListeners() {
+//     loginForm.addEventListener('submit', (ev) => {
+//         ev.preventDefault();
+//         handleRequest();
+//     });
+// }
 
 function handleRequest() {
+    let loginForm = document.getElementById("login-form");
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4) {
