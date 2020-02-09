5de9f5bb4b86eb17551918d2c7c055025f305b4f
dminiail <dminiail@cisco.com>
2020-02-09 12:00:48 +0200

    login js

--- lab3/view/static/login.js
+++ lab3/view/static/login.js
@@ -18,7 +18,7 @@ function successHandler(xhttp) {
     alert("You are logged in. CONGRATS");
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4) {
-            if (!this.status === 200) {
+            if (this.status !== 200) {
                 failureHandler(xhttp);
             }
         }
