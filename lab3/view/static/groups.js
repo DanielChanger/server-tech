1e9991fcb9f394441948adbaa943fdeae138fbbc
dminiail <dminiail@cisco.com>
2020-02-09 13:18:09 +0200

    debug

--- lab3/view/static/groups.js
+++ lab3/view/static/groups.js
@@ -19,7 +19,7 @@ function getGroups() {
 
 
 function successHandler(xhttp) {
-    const groups = JSON.parse(xhttp.responseText);
+    const groups = JSON.parse(xhttp.responseText)["groups"];
     groups.forEach((group) => {
         let groupNode = document.createElement("p");
         let groupNumber = document.createTextNode(group.number);
