bc4e6ca03b89665cd687067bd55fae37c32c5a4f
dminiail <dminiail@cisco.com>
2020-02-09 13:15:25 +0200

    debug

--- lab3/view/static/groups.js
+++ lab3/view/static/groups.js
@@ -19,8 +19,14 @@ function getGroups() {
 
 
 function successHandler(xhttp) {
-    console.log(JSON.parse(xhttp.responseText));
-    console.log(JSON.parse(xhttp.responseText).results);
+    const groups = JSON.parse(xhttp.responseText).groups;
+    groups.forEach((group) => {
+        let groupNode = document.createElement("p");
+        let groupNumber = document.createTextNode(group.number);
+        groupNode.appendChild(groupNumber);
+        let groupsDiv = document.getElementById("groups");
+        groupsDiv.appendChild(groupNode);
+    });
 }
 
 function failureHandler(xhttp) {
