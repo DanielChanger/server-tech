2f7b516fe23c2e36dc7585355232e14799f54c63
dminiail <dminiail@cisco.com>
2020-02-09 14:44:14 +0200

    debug

--- lab3/view/static/groups.js
+++ lab3/view/static/groups.js
@@ -23,10 +23,28 @@ function successHandler(xhttp) {
     groups.forEach((group) => {
         let groupNode = document.createElement("p");
         let groupNumber = document.createTextNode(group.number);
+        groupNode.addEventListener('click', () => {
+            getGroupInfo(groupNumber);
+        });
         groupNode.appendChild(groupNumber);
         let groupsDiv = document.getElementById("groups");
         groupsDiv.appendChild(groupNode);
     });
+}
+
+function getGroupInfo(groupNum) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                successHandler(this);
+            } else {
+                failureHandler(this);
+            }
+        }
+    };
+    xhttp.open("GET", "https://localhost/lab3/controller/groups.php?number=" + groupNum, true);
+    xhttp.send();
 }
 
 function failureHandler(xhttp) {
