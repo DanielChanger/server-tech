e49d6dd14b7f293893256c0666a11fca1c642d6e
dminiail <dminiail@cisco.com>
2020-02-09 12:52:48 +0200

    login js

--- lab3/controller/groups.php
+++ lab3/controller/groups.php
@@ -5,12 +5,14 @@ require_once(__DIR__ . '/../filter/sessionFilter.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
+    require(__DIR__ . '/../view/groups.html');
+
     $groupsService = new GroupsService();
 
     if ((bool) $_SESSION['isAdmin']) {
         $groups = $groupsService->getAllGroupsNumbers();
         $response = ['groups:' => $groups];
-        
+
         header("HTTP/1.1 200 OK");
         echo json_encode($response);
 
