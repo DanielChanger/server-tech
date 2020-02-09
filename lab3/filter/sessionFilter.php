8a33e1f9957c5c738dc94322bd61e9344fd85321
dminiail <dminiail@cisco.com>
2020-02-09 12:18:31 +0200

    login js

--- lab3/filter/sessionFilter.php
+++ lab3/filter/sessionFilter.php
@@ -2,7 +2,9 @@
 
 print_r($_SESSION);
 
-if (isset($_SESSION)) {
+if (!isset($_SESSION)) { session_start(); }
+
+if (!isset($_SESSION['isAdmin'])) {
     header("HTTP/1.1 403 Forbidden");
     die();
 }
